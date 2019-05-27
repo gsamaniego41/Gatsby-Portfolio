159b0d112f1057b031bdab8d012120c24a50bbc6
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-05-27 15:26:45 -0700

    Blog, About, Contact starter pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>I can code and I can design</p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>Email: gsamaniego41@gmail.com</p>
+      <p>GitHub: gsamaniego41</p>
+    </div>
+  )
+}
+
+export default Contact
