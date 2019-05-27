d3c234c700ad8c6baa451aaec997867c25febe79
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-05-27 15:35:27 -0700

    Linking between pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const Home = () => {
   return (
     <div>
       <h1>Gabriel Samaniego</h1>
       <h2>Creative Full Stack Developer</h2>
+      <p>
+        {/* Need a developer? <a href="/contact">Contact Me.</a> */}
+        Need a developer? <Link to="/contact">Contact Me.</Link>
+      </p>
     </div>
   )
 }
