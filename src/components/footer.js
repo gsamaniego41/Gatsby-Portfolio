92f916243dd5f6f176be3988953b810c8e9040fe
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-05-27 19:27:57 -0700

    Removes warning in footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ import footerStyles from "./footer.module.scss"
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className={footerStyles.footer}>
       <p>Created by Gabe Samaniego, &copy; 2019</p>
     </footer>
   )
