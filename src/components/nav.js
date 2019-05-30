bd888b7a9879f1226745d11e5763d644af9a2287
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-05-30 08:44:15 -0700

    Nav styles - Undoing CSS module.

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,27 +1,25 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import navStyles from "./nav.module.scss"
-
 const NavBar = () => {
   return (
     <header>
       <nav>
-        <div className={navStyles.navbar}>
+        <div className="navbar">
           <Link to="/" exact>
             G/S
           </Link>
-          <div className={navStyles.navlinks}>
-            <Link activeClassName={navStyles.active_nav_item} to="/">
+          <div className="navlinks">
+            <Link activeClassName="active_nav_item" to="/">
               Home
             </Link>
-            <Link activeClassName={navStyles.active_nav_item} to="/about">
+            <Link activeClassName="active_nav_item" to="/about">
               Skills
             </Link>
-            <Link activeClassName={navStyles.active_nav_item} to="/projects">
+            <Link activeClassName="active_nav_item" to="/projects">
               Work
             </Link>
-            <Link activeClassName={navStyles.active_nav_item} to="/contact">
+            <Link activeClassName="active_nav_item" to="/contact">
               Contact
             </Link>
           </div>
