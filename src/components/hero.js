089a0360a23220c66884247188e4a0826c23754d
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-05-30 23:29:18 -0700

    Completes hero section - static

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,7 +1,8 @@
 import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
 import profilePic from "../assets/images/Gabe.jpg"
 import arrow from "../assets/triangle-arrow.png"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const Hero = () => {
   return (
@@ -12,16 +13,35 @@ const Hero = () => {
           <br /> Samaniego
         </h1>
         <h4>
-          I’m a Creative Full Stack Software Engineer I love building fully
-          responsive, pixel-perfect web UIs.
+          A Full Stack Software Engineer <br />
+          who loves building fully responsive,
+          <br /> pixel-perfect web UIs.
         </h4>
       </div>
       <div className="hero_right">
         <img src={profilePic} alt="Gabe" />
         <div className="hero_social_icons">
-          <FontAwesomeIcon icon={["fab", "linkedin"]} />
-          <FontAwesomeIcon icon={["fab", "github"]} />
-          <FontAwesomeIcon icon="envelope" />
+          <a
+            href="https://www.linkedin.com/in/gabriel-samaniego/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={["fab", "linkedin"]} />
+          </a>
+          <a
+            href="https://github.com/gsamaniego41"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={["fab", "github"]} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/gabriel-samaniego/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon="envelope" />
+          </a>
         </div>
       </div>
       <img src={arrow} alt="arrow" className="hero_arrow_down" />
