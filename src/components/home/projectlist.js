2f22c2780ec1aa06a375aa69113529a46b719553
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-05-31 13:00:59 -0700

    Styles project list container

--- src/components/home/projectlist.js
+++ src/components/home/projectlist.js
@@ -5,17 +5,23 @@ import HomeProject from "./project"
 
 const HomeProjectList = () => {
   return (
-    <ul>
-      {projectSummary.map(p => (
-        <HomeProject
-          key={p.id}
-          id={p.id}
-          title={p.title}
-          desc={p.desc}
-          thumb={p.thumb}
-        />
-      ))}
-    </ul>
+    <>
+      <div className="section_title">
+        <h2>Projects</h2>
+        <p>Some of my proudest work</p>
+      </div>
+      <ul className="project_summary_container">
+        {projectSummary.map(p => (
+          <HomeProject
+            key={p.id}
+            id={p.id}
+            title={p.title}
+            desc={p.desc}
+            thumb={p.thumb}
+          />
+        ))}
+      </ul>
+    </>
   )
 }
 
