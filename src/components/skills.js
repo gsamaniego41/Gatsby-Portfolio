b3e06b52b51d38b4ad954980b7cedb14e7ef2cf6
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-05-31 00:03:25 -0700

    Completes skill section styles - desktop

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,18 +1,25 @@
 import React from "react"
 import { skills } from "../data/skills"
+import skillsImg from "../assets/images/skills-img.png"
 
 const Skills = () => {
   return (
     <section className="skills_container">
-      <div className="section_title">
-        <h2>Skills</h2>
-        <p>I code and I design</p>
+      <div>
+        <div className="section_title">
+          <h2>Skills</h2>
+          <p>I code and I design</p>
+        </div>
+        <div className="skill_list">
+          {skills.map(skill => (
+            <div className="skill_item">{skill}</div>
+          ))}
+        </div>
       </div>
-      <div className="skill_list">
-        {skills.map(skill => (
-          <div className="skill_item">{skill}</div>
-        ))}
-      </div>
+      <img
+        src={skillsImg}
+        alt="vector drawing of a text editor and graphic design tool"
+      />
     </section>
   )
 }
