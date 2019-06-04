e1f8a494223def1fec9e108d2bf9463048954770
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-06-04 00:09:59 -0700

    .

--- src/components/home/contact.js
+++ src/components/home/contact.js
@@ -35,7 +35,7 @@ const Contact = () => {
         <input name="bot-field" type="text" className="honeypot" />
         <input name="name" type="text" placeholder="Name*" required />
         <input name="company" type="text" placeholder="Company (Optional)" />
-        <input name="email" type="text" placeholder="Email*" required />
+        <input name="email" type="email" placeholder="Email*" required />
         <textarea
           name="message"
           cols="30"
