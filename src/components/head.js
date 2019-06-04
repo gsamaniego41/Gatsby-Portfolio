3d8d6942a74c84d3e9006a10401fe5c9420b7214
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-06-04 16:28:02 -0700

    Changes favicon using React Helmet

--- src/components/head.js
+++ src/components/head.js
@@ -2,6 +2,9 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
+import favicon16 from "../images/favicon16"
+import favicon32 from "../images/favicon32"
+
 const Head = ({ title }) => {
   const data = useStaticQuery(graphql`
     query {
@@ -12,7 +15,26 @@ const Head = ({ title }) => {
       }
     }
   `)
-  return <Helmet title={`${title} | ${data.site.siteMetadata.title}`} />
+  return (
+    <Helmet
+      title={`${title} | ${data.site.siteMetadata.title}`}
+      link={[
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: `${favicon16}`,
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: `${favicon32}`,
+        },
+        { rel: "shortcut icon", type: "image/png", href: `${favicon32}` },
+      ]}
+    />
+  )
 }
 
 export default Head
